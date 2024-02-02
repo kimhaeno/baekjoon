f24315ca5721eb591005da951c660b7f7565e1de
kimhaeno <128720774+kimhaeno@users.noreply.github.com>
2024-02-02 22:54:35 +0900

    [Silver III] Title: N과 M (1), Time: 232 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,24 @@
+visit = [False for i in range(9)]
+curr = [0 for i in range(9)]
+
+def back(n, m, idx):
+    if idx == m:
+        for i in range(1, m+1):
+            print(curr[i], end=" ")
+        print("")
+        return
+    
+    for i in range(1, n+1):
+        if not visit[i]:
+            curr[idx + 1] = i
+            visit[i] = True
+            back(n, m, idx + 1)
+            visit[i] = False
+    
+
+
+
+
+n, m = map(int, input().split())
+
+back(n, m, 0)
